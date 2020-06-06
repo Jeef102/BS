c36b6380a49f3198825069e60776755936818ad2
Ronan Friedman <friedmanr@rsd407.org>
2020-06-05 18:02:20 -0700

    Code

--- bs.py
+++ bs.py
@@ -1,6 +1,23 @@
-#https://aaronschlegel.me/black-scholes-formula-python.html
+import numpy as np
+import scipy.stats as si
+import sympy as sy
+from sympy.stats import Normal, cdf
+from sympy import init_printing
+init_printing()
 
-import scipy
-import numpy
-print('Hello')
-print('Hello')
+def euro_vanilla_call(S,K,T,r,sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+
+    return call
+def euro_vanilla_put(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+    put = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+
+    return put
+print(euro_vanilla_call(50, 100, 1, 0.05, 0.25))
+print(euro_vanilla_put(50, 100, 1, 0.05, 0.25))
